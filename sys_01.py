ac0374432ec005ffae060f55af48ed27f0ec7ad9
SMK2000 <39550995+SMK2000@users.noreply.github.com>
2020-01-02 15:17:47 +0530

    added stderr, stdout

--- sys_01.py
+++ sys_01.py
@@ -3,4 +3,7 @@ import sys
 print("System version is {}".format(sys.version))
 # Since we're testing on windows
 # print(sys.getwindowsversion)
-# use platform module instead
+# use platform module instead - explained later
+sys.stderr.write('This is stderr test\n')
+sys.stderr.flush()
+sys.stdout.write('This is stdout test\n')
