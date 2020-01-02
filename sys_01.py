76933a9b2c9d84b38595a8c8d1436d300ef81d5a
SMK2000 <39550995+SMK2000@users.noreply.github.com>
2020-01-02 15:06:32 +0530

    Update sys_01.py

--- sys_01.py
+++ sys_01.py
@@ -2,4 +2,5 @@
 import sys
 print("System version is {}".format(sys.version))
 # Since we're testing on windows
-print(sys.getwindowsversion)
+# print(sys.getwindowsversion)
+# use platform module instead
