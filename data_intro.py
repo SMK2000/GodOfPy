b404dedcf25726c6124ddf603bd6ae8c50cac426
SMK2000 <39550995+SMK2000@users.noreply.github.com>
2020-01-02 14:29:09 +0530

    data_intro.py ; io_01.py
    Added two .py files :
    1. Introduction to data types
    2. Very basic I/O

---
+++ data_intro.py
@@ -0,0 +1,8 @@
+# Data handling with Python 
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.random.normal(5.0, 1.0, 100000)
+
+plt.hist(x, 100)
+plt.show()
---
+++ io_01.py
@@ -0,0 +1,11 @@
+# Input / Output
+import time
+print("Hello.")
+time.sleep(2)
+print("Type your favourite number")
+a = int(input())
+if (a%2)==0:
+   print("Even")
+else:
+ print("odd")
+
