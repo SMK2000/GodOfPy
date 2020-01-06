3108d235c273627c3d4974ba44e90572a44753a2
SMK2000 <39550995+SMK2000@users.noreply.github.com>
2020-01-06 20:09:28 +0530

    wb replaced by w in open() method

--- File_io_01.py
+++ File_io_01.py
@@ -1,5 +1,5 @@
 # Open a file
-fo = open("foo.txt", "wb")
+fo = open("foo.txt", "w") #writes to foo.txt in "w" mode , i.e. in write mode
 fo.write( "Python is a great language.\nYeah its great!!\n")
 
 # Close opend file
